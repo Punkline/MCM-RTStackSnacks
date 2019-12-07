694017c9b7805fb291ffa902043c47770d86a3b9
Punkline <gasbagx@gmail.com>
2019-12-07 05:50:33 -0800

    Fixed error in blaba.s

--- MCM/blaba.s
+++ MCM/blaba.s
@@ -1,13 +1,13 @@
 .ifndef blaba.included
 blaba.included=1
 .macro bla, a, b
-  .ifb \b;lis r0, \a \@;ori r0, r0, \a \@;mtlr r0;blrl
-  .else;  lis \a, \b \@;ori \a, \a, \b \@;mtlr \a;blrl
+  .ifb \b;lis r0, \a @h;ori r0, r0, \a @l;mtlr r0;blrl
+  .else;  lis \a, \b @h;ori \a, \a, \b @l;mtlr \a;blrl
   .endif
 .endm
 .macro ba, a, b
-  .ifb \b;lis r0, \a \@;ori r0, r0, \a \@;mtctr r0;bctr
-  .else;  lis \a, \b \@;ori \a, \a, \b \@;mtctr \a;bctr
+  .ifb \b;lis r0, \a @h;ori r0, r0, \a @l;mtctr r0;bctr
+  .else;  lis \a, \b @h;ori \a, \a, \b @l;mtctr \a;bctr
   .endif
 .endm
 .endif
